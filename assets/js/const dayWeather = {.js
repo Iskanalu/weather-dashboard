565dd761c761fe80038ca0a5fbfedfe2c7fa8fb1
Isis Nava <navalunaik@gmail.com>
2024-05-24 21:14:25 -0700

    functional app

---
+++ assets/js/const dayWeather = {.js
@@ -0,0 +1,7 @@
+const dayWeather = {
+    date: '12-20-2024',
+    wind: '200',
+    humidity: 50,
+    temp: '50',
+    icon: 'sun'
+};
---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+// agregar el evento del click del botton search y ejecutar la funcion de getData adentro.
+const searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  getData();
+});
+//  function getWeatherIcon
+
+//  if clouds then return emojiClouds
+//  if sun then return emojiSun
+//  etc etc Rain, Snow, Clouds, Clear
+
+async function getData() {  
+
+  // obtener el valor del input com id=city
+    const city = document.getElementById('city').value;
+
+    const geocodeRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=76d841795ac608aab43b884022bf69ec`
+      );
+
+    const geoCoordenates = await geocodeRes.json();
+
+    console.log(geoCoordenates);
+
+    const lat = geoCoordenates.coord.lat;
+    const lon = geoCoordenates.coord.lon;
+
+    const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=76d841795ac608aab43b884022bf69ec`
+      );
+    
+      const weatherInfo = await res.json();
+      const cityWeather = {
+        cityName: weatherInfo.city.name ,
+        forecast: weatherInfo.list
+        .filter((weather) => weather.dt_txt.includes('09:00:00'))
+        .map((weather) => {
+          return {
+            date: new Date(weather.dt_txt).toLocaleDateString("en-US"),
+            wind: weather.wind.speed,
+            humidity: weather.main.humidity,
+            temp: Math.round(((weather.main.temp -273.15) * 9/5 + 32)),
+            icon: getWeatherIcon(weather.weather[0].main)
+          }
+        })          
+      };
+      console.log('cityWeather > ', cityWeather);
+      function getWeatherIcon(main) {
+        if (main === 'Clear') {
+            return "☀️";
+          } else if (main === 'Clouds'){
+            return "💨";
+          } else if(main === 'Snow'){
+            return "❄️";
+          } else if(main === 'Rain'){
+            return "🌧️";   
+          }
+          return main;
+      }
+   
+   document.getElementsByClassName('current-city')[0].innerHTML = `
+    <h3>${cityWeather.cityName}</h3>
+    <p>${cityWeather.forecast[0].icon}</p>
+    <p>Temp: ${cityWeather.forecast[0].temp} °F</p>
+    <p>Wind: ${cityWeather.forecast[0].wind} MPH</p>
+    <p>Humidty: ${cityWeather.forecast[0].humidity} %</p>`;
+
+    document.getElementById('weather-id').innerHTML = '';
+        
+    cityWeather.forecast.forEach(w => {
+      const dayWeather = document.createElement('div');
+      dayWeather.classList.add('day-forecast');
+      dayWeather.classList.add('col-sm-2');
+      dayWeather.innerHTML = `
+        <h5>${w.date}</h5>
+        <p>${w.icon}</p>
+        <p>Temp: ${w.temp} °F</p>
+        <p>Wind: ${w.wind} MPH</p>
+        <p>Humidty: ${w.humidity} %</p>
+    `;
+      document.getElementById('weather-id').appendChild(dayWeather);
+  });
+}
